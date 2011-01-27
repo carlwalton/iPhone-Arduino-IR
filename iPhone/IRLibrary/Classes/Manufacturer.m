02928bee5ede0d6be13c90770dbab38a6e301da0
Carl <carl.walton@gmail.com>
2011-01-27 11:43:48 +0000

    Test

--- iPhone/IRLibrary/Classes/Manufacturer.m
+++
@@ -1,9 +0,0 @@
-#import "Manufacturer.h"
-#import "Model.h"
-
-@implementation Manufacturer
-
-@dynamic Name;
-@dynamic ManufacturerToModel;
-
-@end
--- iPhone/IRLibrary/CoreDataHelper.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-#import <CoreData/CoreData.h>
-@interface CoreDataHelper : NSObject {
-
-}
-+(NSMutableArray *) getObjectsFromContext: (NSString*) entityName : (NSString*) sortKey : (BOOL) sortAscending : (NSManagedObjectContext *) managedObjectContext;
-+(NSMutableArray *) searchObjectsInContext: (NSString*) entityName : (NSPredicate *) predicate : (NSString*) sortKey : (BOOL) sortAscending : (NSManagedObjectContext *) managedObjectContext;
-@end
